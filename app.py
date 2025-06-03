5030fdcea7e0017b8d94737257889e9bb9ab1493
tuongvi0703 <vile.88279@gmail.com>
2025-06-03 16:45:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(_name_)
 
 @app.route('/')
-def home();
-    return 'Xin chao Flask'
+def home():
+    return 'Xin chào bạn đến với Flask'
 
 if _name_=='_main_':
    app.run(host='0.0.0.0', port=10000)
